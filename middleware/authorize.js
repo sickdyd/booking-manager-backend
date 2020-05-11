4d7eea3a903dab3ae084ba73a1c6eff27c617440
sickdyd <roberto.reale.ja@gmail.com>
2020-05-11 10:25:08 +0900

    Added sliding session jwt

--- middleware/authorize.js
+++ middleware/authorize.js
@@ -1,5 +1,6 @@
 "use strict";
 const jwt = require("jsonwebtoken");
+const { User } = require("../models/user");
 const moment = require("moment");
 const config = require("config");
 const errorHandler = require("../errors/errorHandler")
@@ -9,7 +10,7 @@ const errorHandler = require("../errors/errorHandler")
  * Proceeds in the pipeline if token is valid
  */
 
-function authorize(req, res, next) {
+async function authorize(req, res, next) {
 
   const token = req.header("x-auth-token");
   if (!token) return errorHandler(res, "NO_TOKEN");
@@ -17,13 +18,18 @@ function authorize(req, res, next) {
   try {
     const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
 
-    console.log(decoded);
-
     if (decoded.exp > moment.unix()) {
       return errorHandler(res, "INVALID_TOKEN");
     }
 
     req.user = decoded;
+
+    const user = await User.findById(decoded._id);
+    if (!user) return errorHandler(res, "UNAUTHORIZED");
+    const newToken = user.generateAuthToken();
+
+    res.set("Access-Control-Expose-Headers", "x-auth-token");
+    res.set("x-auth-token", newToken);
 
     next();
   }
